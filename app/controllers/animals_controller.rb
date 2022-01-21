ea816aca10483ef76068198dd5aa904ae36dc714
rccalpito <rccalpito@gmail.com>
2022-01-21 13:38:02 -0800

    first commit

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,37 @@
+class AnimalsController < ApplicationController
+    # Look at the controller verbs
+    def index
+        animals = Animal.all
+        render json: animals
+    end
+    def show
+        animal = Animal.find(params[:id])
+        render json: animal
+    end
+    def create
+        animal = Animal.create(animal_params)
+        # if animal.valid?
+        #     render json: animal
+        # else
+        #     render json: animal.error
+        # end
+    end
+    def new
+        animal = Animal.new
+    end
+    def update
+        animal = Animal.find(params[:id])
+        animal.update(animal_params)
+    end
+    def edit
+        animal = Animal.find(params[:id])
+    end
+    def destroy
+    end
+
+    private
+    def animal_params
+        params.require(:animal).permit(:common_name,:latin_name,:kingdom)
+    end
+
+end
